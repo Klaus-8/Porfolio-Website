ece89765faf2f3d84f15fc2864a7c705386fcf81
Shreyas <shreyasshahu786@gmail.com>
2023-07-07 12:44:56 +0530

    Updated Experiences Section

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -33,8 +33,7 @@ const Header = () => {
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer</p>
-            <p className="p-text">Data Engineer</p>
+            <p className="p-text">Software Developer</p>
           </div>
         </div>
       </motion.div>
